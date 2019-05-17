cc0752c2821bfd627672a09f2a5f054522a93960
Wisniewski <wisniewk@emea.roche.com>
2019-05-17 16:19:15 +0200

    Refactoring

--- src/main/java/pl/kwi/chrisblog/controllers/navigation/UserTypeController.java
+++ src/main/java/pl/kwi/chrisblog/controllers/navigation/UserTypeController.java
@@ -1,4 +1,4 @@
-package pl.kwi.chrisblog.controllers;
+package pl.kwi.chrisblog.controllers.navigation;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,16 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value="/user")
 public class UserTypeController {
 	
-	private final static String[] topics = {"dance", "it"};
+	private final static String[] TOPICS = {"dance", "it"};
 	
 	@ModelAttribute("topics")
     public List<String> initTopics() {
-        return Arrays.asList(topics);
+        return Arrays.asList(TOPICS);
     }
 
 	@RequestMapping
 	public String displayPage() {
-		return "userType";
+		return "navigation/userType";
 	}
 
 
